27f4eb7af851da180baa8fb33311b2aeda6aedbb
Koji Ota <afterjnih@gmail.com>
2022-07-22 00:46:14 +0900

    add bit request

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,7 +3,7 @@ package example
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Directives.{complete, get, post, path, concat}
 import akka.stream.ActorMaterializer
 
 import scala.io.StdIn
@@ -15,14 +15,30 @@ object App {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
     val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      concat(
+        path("hello") {
+          get {
+            complete(
+              HttpEntity(
+                ContentTypes.`text/html(UTF-8)`,
+                "<h1>Say Hello to akka-http</h1>"
+              )
+            )
+          }
+        },
+        path("bit") {
+          post {
+            complete(
+              HttpEntity(
+                ContentTypes.`text/html(UTF-8)`,
+                "<h1>this is bit response</h1>"
+              )
+            )
+          }
         }
-      }
+      )
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
 
     println("hello world2")
     StdIn.readLine() // let it run until user presses return
